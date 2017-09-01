7cdd84a2cf2b0f2db5bab1b21cd764e3809eac0b
anskrish <anskrishcode@gmail.com>
2017-09-01 17:57:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal krishna new window.
+      // Prints "Hello, World" in the terminal krishna new widndow.
       System.out.println("Hello, World");
    }
 }
