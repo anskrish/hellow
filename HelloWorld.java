f4186c74832ee42d642c0b4136417464cd0d4327
anskrish <anskrishcode@gmail.com>
2017-09-01 17:59:58 +0530

    new

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal krishna new widndow.
+      // Prints "Hello, World" in the terminal krishna new widndow.sxs
       System.out.println("Hello, World");
    }
 }
