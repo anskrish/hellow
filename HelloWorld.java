6cb17d24cecb3fc4ad33251852873d4c4850418f
Krishna-Ansible <anskrishcode@gmail.com>
2018-03-04 11:48:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal krishna new widndow.sxs. kris
-      System.out.println("Hello, World krishna");
+      System.out.println("Hello, World is krishna");
    }
 }
